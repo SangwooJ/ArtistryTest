cb23353da415a1a4657e89ee58a83bfbdfbba61b
sangwooJ <jeonsang-u@jeonsang-uui-MacBookPro.local>
2023-05-16 19:14:12 +0900

    making result page done clean some log

--- js/share.js
+++ js/share.js
@@ -3,7 +3,7 @@ const url = 'https://artistrytest.netlify.app/';
 function setShare(){
   //var resultImg = document.querySelector('#resultImg');
   var resultAlt = 0;
-  const shareTitle = '십이간지 연애유형 결과';
+  const shareTitle = '예술성테스트 결과';
   const shareDes = infoList[resultAlt].name;
   const shareImage = url + 'img/main.png';
   //const shareURL = url + 'page/result-' + resultAlt + '.html';
@@ -33,13 +33,12 @@ function setShare(){
   
   // URL 생성
   const finalURL = buildURL(baseURL, params);
-  console.log(finalURL);
-
-  console.log(Kakao);
-  console.log(shareTitle);
-  console.log(shareDes);
-  console.log(shareImage);
-  console.log(shareURL);
+  //console.log(finalURL);
+  //console.log(Kakao);
+  //console.log(shareTitle);
+  //console.log(shareDes);
+  //console.log(shareImage);
+  //console.log(shareURL);
 
 
   Kakao.Share.sendDefault({
